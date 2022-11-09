149326e9837b6f75d3d58a6a050b1b3c69808b71
acferten <gowiththeirname@gmail.com>
2022-11-09 13:09:18 +0700

    Домашнее задание абстрактный класс

--- index.php
+++ index.php
@@ -1,29 +1,12 @@
 <?php
 
-class Cat
+abstract class HumanAbstract
 {
-    private string $name;
-    private string $color;
+    protected string $name;
 
-    public function __construct(string $name, string $color)
+    public function __construct(string $name)
     {
         $this->name = $name;
-        $this->color = $color;
-    }
-
-    public function sayHello()
-    {
-        echo 'Привет! Меня зовут ' . $this->name . '.' . ' Мой окрас ' . $this->color . '. ';
-    }
-
-    public function setName(string $name)
-    {
-        $this->name = $name;
-    }
-
-    public function setColor(string $color)
-    {
-        $this->color = $color;
     }
 
     public function getName(): string
@@ -31,13 +14,58 @@ class Cat
         return $this->name;
     }
 
-    public function getColor(): string
+    abstract public function getGreetings(): string;
+
+    abstract public function getMyNameIs(): string;
+
+    public function introduceYourself(): string
     {
-        return $this->color;
+        return $this->getGreetings() . '! ' . $this->getMyNameIs() . ' ' . $this->getName() . '.';
     }
 }
 
-$cat1 = new Cat('Снежок', 'Белый');
-$cat2 = new Cat('Барсик', 'Рыжий');
-echo $cat1->sayHello();
-echo $cat2->sayHello();
+class RussianHuman extends HumanAbstract
+{
+    public function getGreetings(): string
+    {
+        return 'Привет';
+    }
+
+    public function getMyNameIs(): string
+    {
+        return $this->name;
+    }
+
+    public function introduceYourself(): string
+    {
+        $intro = $this->getGreetings() . '! ' . 'Меня зовут ' . $this->getMyNameIs();
+        return $intro;
+    }
+
+}
+
+class EnglishHuman extends HumanAbstract
+{
+    public function getGreetings(): string
+    {
+        return ' Hello';
+    }
+
+    public function getMyNameIs(): string
+    {
+        return $this->name;
+    }
+
+    public function introduceYourself(): string
+    {
+        $intro = $this->getGreetings() . '! ' . 'My name is ' . $this->getMyNameIs();
+        return $intro;
+    }
+
+}
+
+
+$rus = new RussianHuman('Иван');
+print_r($rus->introduceYourself());
+$eng = new EnglishHuman('John');
+print_r($eng->introduceYourself());
